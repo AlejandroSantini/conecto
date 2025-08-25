0edb674aa6b5ed3e97d607a5877c35b50a75498d
AlejandroSantini <alejandro.j.santini@gmail.com>
2025-08-25 00:40:23 -0300

    Hotfix routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import PostsPage from './pages/Posts/Test';
-import PostDetail from './pages/PostDetail/Test';
+import PostsPage from './pages/Posts/Posts';
+import PostDetail from './pages/PostDetail/PostDetail';
 
 function App() {
     return (
